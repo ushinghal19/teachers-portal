261bebbd8803f08883c687aa0c33002fc847af32
Evan Kanter <evankanter28@gmail.com>
2020-10-28 00:54:46 -0400

    Added documentation, demo, and working app template for companion app. resolves #4

---
+++ companion-app/src/companion_app.py
@@ -0,0 +1,48 @@
+
+import eventlet
+import requests
+import socketio
+import json
+
+sio = socketio.Server(cors_allowed_origins='*')
+app = socketio.WSGIApp(sio)
+
+TEST_ENDPOINT = r'https://hookb.in/lJPDz0kkRYcrXXZWdpgN'
+
+@sio.event
+def connect(sid: int, environ: dict) -> None:
+    """
+    Handler Method to process connection to Hypatia API.
+    """
+    print('connect ', sid)
+
+
+@sio.on('expressions')
+def message_expressions(sid: int, data: str) -> None:
+    """Handler method to process `expression` events from the Hypatia API"""
+    print('expressions:\n', data)
+    record = json.loads(data)
+    # TODO: Decide what to do with the expressions
+    r = requests.post(TEST_ENDPOINT, data=data)
+    print(r.text)
+
+
+@sio.on('result')
+def message_result(sid: int, data: str) -> None:
+    """Handler method to process `result` events from the Hypatia API"""
+    print('result:\n', data)
+    record = json.loads(data)
+    print(record)
+    # TODO: Decide what to do with this
+    r = requests.post(TEST_ENDPOINT, data=data)
+    print(r.text)
+
+
+@sio.event
+def disconnect(sid: int) -> None:
+    """Handler method to process `disconnect` events from the Hypatia API"""
+    print('disconnect ', sid)
+
+
+if __name__ == '__main__':
+    eventlet.wsgi.server(eventlet.listen(('localhost', 3333)), app)
