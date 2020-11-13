2387d5002c13e79003b91fb02dbab74d50656aeb
Winston <winston132020@gmail.com>
2020-11-13 13:31:26 -0500

    added primary keys for ids

--- server/tp_server/tp/models.py
+++ server/tp_server/tp/models.py
@@ -3,24 +3,24 @@ from django.db import models
 # Create your models here.
 
 class Error(models.Model):
-    error_id = models.CharField(max_length=1000)
+    error_id = models.CharField(max_length=1000, primary_key = True)
     error_type = models.CharField(max_length=100)
     student_name = models.CharField(max_length=100)
 
 class Problem(models.Model):
-    problem_id = models.PositiveSmallIntegerField()
+    problem_id = models.PositiveSmallIntegerField(primary_key = True)
     errors = models.ArrayField(
         model_container = Error
     )
 
 class Assignment(models.Model):
-    assignment_id = models.PositiveSmallIntegerField()
+    assignment_id = models.PositiveSmallIntegerField(primary_key = True)
     questions = models.ArrayField(
         model_container = Problem
     )
 
 class Teacher(models.Model):
-    teacher_id = models.CharField(max_length=100)
+    teacher_id = models.CharField(max_length=100, primary_key = True)
     assignments = models.ArrayField(
         model_container = Assignment
     )
