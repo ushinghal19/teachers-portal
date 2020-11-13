3fce23b65dd26bace3a05be083270a2001d4321c
Winston <winston132020@gmail.com>
2020-11-13 11:59:02 -0500

    Added database models to models.py

--- server/tp_server/tp/models.py
+++ server/tp_server/tp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Error(models.Model):
+    error_id = models.PositiveBigIntegerField()
+    error_type = models.CharField(max_length=100)
+    student_name = models.CharField(max_length=100)
+
+class Question(models.Model):
+    question_number = models.PositiveSmallIntegerField()
+    errors = models.ArrayField(
+        model_container = Error
+    )
+
+class Assignment(models.Model):
+    assignment_id = models.PositiveSmallIntegerField()
+    questions = models.ArrayField(
+        model_container = Question
+    )
+
+class Teacher(models.Model):
+    teacher_id = models.CharField(max_length=100)
+    assignments = models.ArrayField(
+        model_container = Assignment
+    )
