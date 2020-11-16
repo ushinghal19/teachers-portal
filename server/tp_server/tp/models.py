c35963c5b32805d0722061fc329a65082b773a49
Winston <winston132020@gmail.com>
2020-11-16 17:13:13 -0500

    fixed ordering problem

--- server/tp_server/tp/models.py
+++ server/tp_server/tp/models.py
@@ -3,18 +3,10 @@ from djongo.models.fields import ArrayField
 
 # Create your models here.
 
-class Error(models.Model):
-    error_id = models.CharField(max_length=1000, primary_key=True)
-    error_type = models.CharField(max_length=100)
-    student_name = models.CharField(max_length=100)
-    problem_number = models.ForeignKey(Problem, on_delete=models.CASCADE)
-    assignment_id = models.ForeignKey(Assignment, on_delete=models.CASCADE)
-    teacher_id = models.ForeginKey(Teacher, on_delete=models.CASCADE)
-
-class Problem(models.Model):
-    problem_id = models.PositiveSmallIntegerField(primary_key = True)
-    errors = models.ArrayField(
-        model_container = Error
+class Teacher(models.Model):
+    teacher_id = models.CharField(max_length=100, primary_key = True)
+    assignments = models.ArrayField(
+        model_container = Assignment
     )
 
 class Assignment(models.Model):
@@ -23,8 +15,16 @@ class Assignment(models.Model):
         model_container = Problem
     )
 
-class Teacher(models.Model):
-    teacher_id = models.CharField(max_length=100, primary_key = True)
-    assignments = models.ArrayField(
-        model_container = Assignment
+class Problem(models.Model):
+    problem_id = models.PositiveSmallIntegerField(primary_key = True)
+    errors = models.ArrayField(
+        model_container = Error
     )
+
+class Error(models.Model):
+    error_id = models.CharField(max_length=1000, primary_key=True)
+    error_type = models.CharField(max_length=100)
+    student_name = models.CharField(max_length=100)
+    problem_number = models.ForeignKey(Problem, on_delete=models.CASCADE)
+    assignment_id = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    teacher_id = models.ForeginKey(Teacher, on_delete=models.CASCADE)
