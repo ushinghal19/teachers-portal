d294878b549bfe648b9a05294467cdf9eea10f6c
Winston <winston132020@gmail.com>
2020-11-16 17:09:45 -0500

    switched to more robust model

--- server/tp_server/tp/models.py
+++ server/tp_server/tp/models.py
@@ -7,6 +7,24 @@ class Error(models.Model):
     id = models.CharField(max_length=1000, primary_key=True)
     error_type = models.CharField(max_length=100)
     student_name = models.CharField(max_length=100)
-    problem_number = models.PositiveSmallIntegerField()
-    assignment_id = models.PositiveSmallIntegerField()
-    teacher_id = models.CharField(max_length=100)
+    problem_number = models.ForeignKey(Problem, on_delete=models.CASCADE)
+    assignment_id = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    teacher_id = models.ForeginKey(Teacher, on_delete=models.CASCADE)
+
+class Problem(models.Model):
+    problem_id = models.PositiveSmallIntegerField(primary_key = True)
+    errors = models.ArrayField(
+        model_container = Error
+    )
+
+class Assignment(models.Model):
+    assignment_id = models.CharField(max_length=1000, primary_key = True)
+    problems = models.ArrayField(
+        model_container = Problem
+    )
+
+class Teacher(models.Model):
+    teacher_id = models.CharField(max_length=100, primary_key = True)
+    assignments = models.ArrayField(
+        model_container = Assignment
+    )
