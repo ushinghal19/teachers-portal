785dbf3a00b9bdf470372357d3ea98055ccacfe0
Utsav Shinghal <ushinghal19@gmail.com>
2020-11-17 05:20:13 +0800

    fixed naming convention

--- server/tp_server/tp/statistics.py
+++ server/tp_server/tp/statistics.py
@@ -4,13 +4,8 @@ from models import Error
 # assignment = Assignment()
 
 # Aggregate Errors:
-<<<<<<< HEAD
 def aggregate_errors(assignment: Assignment) -> int:
     """ Takes an assignment object and returns the total
-=======
-def aggregateErrors(assignment: int) -> int:
-    """ Takes an assignment id (int) and returns the total
->>>>>>> 47362f8a80065a3e0da81a38b95f2674ae2a57d0
         number of errors made by all students for that
         assignment
 
@@ -20,13 +15,8 @@ def aggregateErrors(assignment: int) -> int:
 
 
 # Per Problem Error:
-<<<<<<< HEAD
 def problem_errors(assignment: Assignment) -> dict:
     """ Takes an assignment object and returns a
-=======
-def problemErrors(assignment: int) -> dict:
-    """ Takes an assignment id (int) and returns a
->>>>>>> 47362f8a80065a3e0da81a38b95f2674ae2a57d0
         dictionary of each of the problems in the 
         assignment and the number of errors all
         students made on that problem.
@@ -51,13 +41,8 @@ def problemErrors(assignment: int) -> dict:
     # return problems
 
 # Type of Error:
-<<<<<<< HEAD
 def type_of_errors(assignment: Assignment) -> dict:
     """ Takes an assignment object and returns a 
-=======
-def typeOfErrors(assignment: int) -> dict:
-    """ Takes an assignment id (int) and returns a 
->>>>>>> 47362f8a80065a3e0da81a38b95f2674ae2a57d0
         dictionary of each of the types of errors 
         made in the assignment, and how often they
         were made by all students. 
@@ -79,13 +64,8 @@ def typeOfErrors(assignment: int) -> dict:
     # return errors
 
 # Best Students and Worst Students:
-<<<<<<< HEAD
 def students_by_errors(assignment: Assignment) -> list:
     """ Takes an assigment object and creates a
-=======
-def studentsByErrors(assignment: int) -> list:
-    """ Takes an assigment id (int) and creates a
->>>>>>> 47362f8a80065a3e0da81a38b95f2674ae2a57d0
         sorted list of students ranked by how few
         to how many errors they made based on the #
         of calls.
