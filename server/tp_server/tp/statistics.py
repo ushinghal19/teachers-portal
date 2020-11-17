5565f9e92ff0e707f70176beb8707b65d609a207
Utsav Shinghal <ushinghal19@gmail.com>
2020-11-17 08:24:35 +0800

    updated statistics for new models

--- server/tp_server/tp/statistics.py
+++ server/tp_server/tp/statistics.py
@@ -1,4 +1,4 @@
-from .models import Error
+from .models import Error, Problem, Assignment, Teacher
 
 # Req: teacher_id = 100, assignment_id = 50 Give me Stats
 # assignment = Assignment()
@@ -10,8 +10,8 @@ def aggregate_errors(assignment: Assignment) -> int:
         assignment
 
     """
-    return len(Error.objects.filter(assignment_id = assignment))
-    # return sum(len(problem.errors) for problem in assignment.problems)
+    # return len(Error.objects.filter(assignment_id = assignment))
+    return sum(len(problem_id.errors) for problem_id in assignment.problems)
 
 
 # Per Problem Error:
@@ -21,24 +21,24 @@ def problem_errors(assignment: Assignment) -> dict:
         assignment and the number of errors all
         students made on that problem.
     """
-    problems = {}
-    for i in Error.objects.filter(assignment_id = assignment):
-        if i.problem_number in problems:
-            problems[i.problem_number] += 1
-        else:
-            problems[i.problem_number] = 1
-    return problems
-
     # problems = {}
-    # countError = 0
-    # problemNumber = 1
-
-    # for problem in assignment.problems:
-    #     for error in problem.errors:
-    #         countError += 1
-    #     problems[problemNumber] = countError
-    #     problemNumber += 1
+    # for i in Error.objects.filter(assignment_id = assignment):
+    #     if i.problem_number in problems:
+    #         problems[i.problem_number] += 1
+    #     else:
+    #         problems[i.problem_number] = 1
     # return problems
+
+    problems = {}
+    countError = 0
+    problemNumber = 1
+
+    for problem in assignment.problems:
+        for error in problem.errors:
+            countError += 1
+        problems[problemNumber] = countError
+        problemNumber += 1
+    return problems
 
 # Type of Error:
 def type_of_errors(assignment: Assignment) -> dict:
@@ -47,21 +47,21 @@ def type_of_errors(assignment: Assignment) -> dict:
         made in the assignment, and how often they
         were made by all students.
     """
-    errors = {}
-    for i in Error.objects.filter(assignment_id = assignment):
-        if i.error_type in errors:
-            errors[i.error_type] += 1
-        else:
-            errors[i.error_type] = 1
-
     # errors = {}
-    # for problem in assignment.problems:
-    #     for error in problem.errors:
-    #         if error in errors:
-    #             errors[error] += 1
-    #         else:
-    #             errors[error] = 1
-    # return errors
+    # for i in Error.objects.filter(assignment_id = assignment):
+    #     if i.error_type in errors:
+    #         errors[i.error_type] += 1
+    #     else:
+    #         errors[i.error_type] = 1
+
+    errors = {}
+    for problem in assignment.problems:
+        for error in problem.errors:
+            if error in errors:
+                errors[error] += 1
+            else:
+                errors[error] = 1
+    return errors
 
 # Best Students and Worst Students:
 def students_by_errors(assignment: Assignment) -> list:
@@ -70,22 +70,22 @@ def students_by_errors(assignment: Assignment) -> list:
         to how many errors they made based on the #
         of calls.
     """
-    students = {}
-    for i in Error.objects.filter(assignment_id = assignment):
-        if i.student_name in students:
-            students[i.student_name] += 1
-        else:
-            students[i.student_name] = 1
-    sorted_students = sorted(students.items(), key=lambda item: item[1])
-    return sorted_students
-
     # students = {}
-    # for problem in assignment.problems:
-    #     for error in problem.errors:
-    #         if error.student_name in students:
-    #             students[error.student_name] += 1
-    #         else:
-    #             students[error.student_name] = 1
-
-    # sorted_students = sorted(students, key= lambda x: students[x])
+    # for i in Error.objects.filter(assignment_id = assignment):
+    #     if i.student_name in students:
+    #         students[i.student_name] += 1
+    #     else:
+    #         students[i.student_name] = 1
+    # sorted_students = sorted(students.items(), key=lambda item: item[1])
     # return sorted_students
+
+    students = {}
+    for problem in assignment.problems:
+        for error in problem.errors:
+            if error.student_name in students:
+                students[error.student_name] += 1
+            else:
+                students[error.student_name] = 1
+
+    sorted_students = sorted(students, key=lambda x: students[x])
+    return sorted_students
