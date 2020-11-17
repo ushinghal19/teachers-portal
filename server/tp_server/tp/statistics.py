fc27ccbed09bfcd20068b622a4f16e5833455dab
Utsav Shinghal <ushinghal19@gmail.com>
2020-11-17 08:27:34 +0800

    fixed stuff

--- server/tp_server/tp/statistics.py
+++ server/tp_server/tp/statistics.py
@@ -10,8 +10,8 @@ def aggregate_errors(assignment: Assignment) -> int:
         assignment
 
     """
-    # return len(Error.objects.filter(assignment_id = assignment))
     return sum(len(problem_id.errors) for problem_id in assignment.problems)
+    # return len(Error.objects.filter(assignment_id = assignment))
 
 
 # Per Problem Error:
@@ -21,13 +21,6 @@ def problem_errors(assignment: Assignment) -> dict:
         assignment and the number of errors all
         students made on that problem.
     """
-    # problems = {}
-    # for i in Error.objects.filter(assignment_id = assignment):
-    #     if i.problem_number in problems:
-    #         problems[i.problem_number] += 1
-    #     else:
-    #         problems[i.problem_number] = 1
-    # return problems
 
     problems = {}
     countError = 0
@@ -40,6 +33,15 @@ def problem_errors(assignment: Assignment) -> dict:
         problemNumber += 1
     return problems
 
+    # problems = {}
+    # for i in Error.objects.filter(assignment_id = assignment):
+    #     if i.problem_number in problems:
+    #         problems[i.problem_number] += 1
+    #     else:
+    #         problems[i.problem_number] = 1
+    # return problems
+
+
 # Type of Error:
 def type_of_errors(assignment: Assignment) -> dict:
     """ Takes an assignment object and returns a
@@ -47,12 +49,6 @@ def type_of_errors(assignment: Assignment) -> dict:
         made in the assignment, and how often they
         were made by all students.
     """
-    # errors = {}
-    # for i in Error.objects.filter(assignment_id = assignment):
-    #     if i.error_type in errors:
-    #         errors[i.error_type] += 1
-    #     else:
-    #         errors[i.error_type] = 1
 
     errors = {}
     for problem in assignment.problems:
@@ -63,6 +59,13 @@ def type_of_errors(assignment: Assignment) -> dict:
                 errors[error] = 1
     return errors
 
+    # errors = {}
+    # for i in Error.objects.filter(assignment_id = assignment):
+    #     if i.error_type in errors:
+    #         errors[i.error_type] += 1
+    #     else:
+    #         errors[i.error_type] = 1
+
 # Best Students and Worst Students:
 def students_by_errors(assignment: Assignment) -> list:
     """ Takes an assigment object and creates a
@@ -70,14 +73,6 @@ def students_by_errors(assignment: Assignment) -> list:
         to how many errors they made based on the #
         of calls.
     """
-    # students = {}
-    # for i in Error.objects.filter(assignment_id = assignment):
-    #     if i.student_name in students:
-    #         students[i.student_name] += 1
-    #     else:
-    #         students[i.student_name] = 1
-    # sorted_students = sorted(students.items(), key=lambda item: item[1])
-    # return sorted_students
 
     students = {}
     for problem in assignment.problems:
@@ -89,3 +84,12 @@ def students_by_errors(assignment: Assignment) -> list:
 
     sorted_students = sorted(students, key=lambda x: students[x])
     return sorted_students
+
+    # students = {}
+    # for i in Error.objects.filter(assignment_id = assignment):
+    #     if i.student_name in students:
+    #         students[i.student_name] += 1
+    #     else:
+    #         students[i.student_name] = 1
+    # sorted_students = sorted(students.items(), key=lambda item: item[1])
+    # return sorted_students
