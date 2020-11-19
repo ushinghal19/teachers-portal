5b8fd1dcf79f3321d1361967c444219004fe1b43
Omri Daniel <omri.daniel@mail.utoronto.ca>
2020-11-19 14:16:14 -0500

    Completed the average time component styling
    Pair programming with Ekagra to complete component styling

--- front-end/tp-react-app/src/components/dashboard/AverageTime.js
+++ front-end/tp-react-app/src/components/dashboard/AverageTime.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 import './AverageTime.scss'
 import '../login/LoginPage.scss'
-import ListGroup from 'react-bootstrap/ListGroup'
 
 class AverageTime extends Component {
     render() {
-        const averageTime = this.props.averageTime; // TODO: Figure out what the input type is (list, dict, string, tuple)
         return (
             <div className='average-time-box'>
-                <div className="tp-head" style={{fontSize: 35,}}>Average Time Taken</div>
-                <div className="average-time">
-                    On average, students took {averageTime} minutes
-                </div>
+                <div className="tp-head" style={{fontSize: 35,}}>Average Time</div>
+                    <div className="average-time">
+                        Coming Soon
+                    </div>
             </div>
         );
     }
 }
 
-export default TotalErrors;
+export default AverageTime;
