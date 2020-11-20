64166c3fcc9eed4a9218eae02604f2ebb4d748ac
Utsav Shinghal <ushinghal19@gmail.com>
2020-11-21 01:07:58 +0800

    API statistics are now working

--- server/tp_server/tp/object_types.py
+++ server/tp_server/tp/object_types.py
@@ -2,29 +2,30 @@ import graphene
 from graphene_django import DjangoObjectType
 from .models import Error, Problem, Assignment, Teacher
 from .statistics import aggregate_errors, problem_errors, students_by_errors, type_of_errors
+from graphene.types.generic import GenericScalar
 
 
-class StatisticType(graphene.ObjectType):
-    """
-    Object type for statistics that we are gathering
-    """
-
-    aggregate_errors = graphene.Int()
-    problem_errors = graphene.Int()
-    type_of_errors = graphene.JSONString()
-    students_by_errors = graphene.JSONString()
-
-    def resolve_aggregate_errors(parent, info):
-        return aggregate_errors(parent)
-
-    def resolve_problem_errors(parent, info):
-        return problem_errors(parent)
-
-    def resolve_type_of_errors(parent, info):
-        return type_of_errors(parent)
-
-    def resolve_students_by_errors(parent, info):
-        return students_by_errors(parent)
+# class StatisticType(graphene.ObjectType):
+#     """
+#     Object type for statistics that we are gathering
+#     """
+#
+#     aggregate_errors = graphene.Int()
+#     problem_errors = graphene.Int()
+#     type_of_errors = graphene.JSONString()
+#     students_by_errors = graphene.JSONString()
+#
+#     def resolve_aggregate_errors(parent, info):
+#         return aggregate_errors(parent.parent)
+#
+#     def resolve_problem_errors(parent, info):
+#         return problem_errors(parent.parent)
+#
+#     def resolve_type_of_errors(parent, info):
+#         return type_of_errors(parent.parent)
+#
+#     def resolve_students_by_errors(parent, info):
+#         return students_by_errors(parent.parent)
 
 
 class ErrorType(DjangoObjectType):
@@ -36,6 +37,11 @@ class ErrorType(DjangoObjectType):
         model = Error
         fields = ('error_id', 'error_type', 'student_name')
         filter_fields = ['error_id', 'error_type', 'student_name']
+
+    error_type = graphene.String()
+
+    def resolve_error_type(parent, info):
+        return parent.error_type
 
 
 class ProblemType(DjangoObjectType):
@@ -66,14 +72,27 @@ class AssignmentType(DjangoObjectType):
 
     # THIS MIGHT BECOME AN ISSUE
     problems_below = graphene.List(ProblemType)
-    statistic = graphene.Field(StatisticType)
+    # statistic = graphene.Field(StatisticType)
+
+    aggregate_errors = graphene.Int()
+    problem_errors = GenericScalar()
+    type_of_errors = GenericScalar()
+    students_by_errors = GenericScalar()
 
     def resolve_problems_below(parent, info):
         return parent.problems.all()
 
+    def resolve_aggregate_errors(parent, info):
+        return aggregate_errors(parent)
 
-    def resolve_statistic(parent, info):
-        return StatisticType(parent=parent)
+    def resolve_problem_errors(parent, info):
+        return problem_errors(parent)
+
+    def resolve_type_of_errors(parent, info):
+        return type_of_errors(parent)
+
+    def resolve_students_by_errors(parent, info):
+        return students_by_errors(parent)
 
 
 class TeacherType(DjangoObjectType):
--- server/tp_server/tp/statistics.py
+++ server/tp_server/tp/statistics.py
@@ -1,22 +1,15 @@
-from .models import Assignment
-
-# Req: _id = 100, assignment_id = 50 Give me Stats
-# assignment = Assignment()
 
 
-# Aggregate Errors:
-def aggregate_errors(assignment: Assignment) -> int:
+def aggregate_errors(assignment) -> int:
     """ Takes an assignment object and returns the total
         number of errors made by all students for that
         assignment
 
     """
-    return sum(len(problem_id.errors) for problem_id in assignment.problems)
-    # return len(Error.objects.filter(assignment_id = assignment))
+    return sum(len(problem.errors.all()) for problem in assignment.problems.all())
 
 
-# Per Problem Error:
-def problem_errors(assignment: Assignment) -> dict:
+def problem_errors(assignment) -> dict:
     """ Takes an assignment object and returns a
         dictionary of each of the problems in the
         assignment and the number of errors all
@@ -24,24 +17,15 @@ def problem_errors(assignment: Assignment) -> dict:
     """
 
     problems = {}
-    problemNumber = 1
+    problem_number = 1
 
-    for problem in assignment.problems:
-        problems[problemNumber] = len(problem.errors)
-        problemNumber += 1
+    for problem in assignment.problems.all():
+        problems[problem_number] = len(problem.errors.all())
+        problem_number += 1
     return problems
 
-    # problems = {}
-    # for i in Error.objects.filter(assignment_id = assignment):
-    #     if i.problem_number in problems:
-    #         problems[i.problem_number] += 1
-    #     else:
-    #         problems[i.problem_number] = 1
-    # return problems
 
-
-# Type of Error:
-def type_of_errors(assignment: Assignment) -> dict:
+def type_of_errors(assignment) -> dict:
     """ Takes an assignment object and returns a
         dictionary of each of the types of errors
         made in the assignment, and how often they
@@ -49,46 +33,29 @@ def type_of_errors(assignment: Assignment) -> dict:
     """
 
     errors = {}
-    for problem in assignment.problems:
-        for error in problem.errors:
-            if error in errors:
-                errors[error] += 1
+    for problem in assignment.problems.all():
+        for error in problem.errors.all():
+            if error.error_type in errors:
+                errors[error.error_type] += 1
             else:
-                errors[error] = 1
+                errors[error.error_type] = 1
     return errors
 
-    # errors = {}
-    # for i in Error.objects.filter(assignment_id = assignment):
-    #     if i.error_type in errors:
-    #         errors[i.error_type] += 1
-    #     else:
-    #         errors[i.error_type] = 1
 
-
-# Best Students and Worst Students:
-def students_by_errors(assignment: Assignment) -> list:
-    """ Takes an assigment object and creates a
+def students_by_errors(assignment) -> dict:
+    """ Takes an assignment object and creates a
         sorted list of students ranked by how few
         to how many errors they made based on the #
         of calls.
     """
 
     students = {}
-    for problem in assignment.problems:
-        for error in problem.errors:
+    for problem in assignment.problems.all():
+        for error in problem.errors.all():
             if error.student_name in students:
                 students[error.student_name] += 1
             else:
                 students[error.student_name] = 1
 
-    sorted_students = sorted(students, key=lambda x: students[x])
-    return sorted_students
-
-    # students = {}
-    # for i in Error.objects.filter(assignment_id = assignment):
-    #     if i.student_name in students:
-    #         students[i.student_name] += 1
-    #     else:
-    #         students[i.student_name] = 1
-    # sorted_students = sorted(students.items(), key=lambda item: item[1])
-    # return sorted_students
+    students = {key: value for key, value in sorted(students.items(), key=lambda item: item[1])}
+    return students
