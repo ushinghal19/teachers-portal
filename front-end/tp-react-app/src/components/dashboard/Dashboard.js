d7df7860acf30d5e4e0bee092bba80e9e64f6a35
Omri Daniel <omri.daniel@mail.utoronto.ca>
2020-11-23 14:07:11 -0500

    Styling changes
    Edited the tphead styling: now has drop shadow and proper rounded edges
    Edited total errors box: centering content. Still need to center vertically though
    Edited dashboard page: background color fixed and modified margins to not cut off the navbar. Still need to figure out why bottom of page is different colour.

--- front-end/tp-react-app/src/components/dashboard/Dashboard.js
+++ front-end/tp-react-app/src/components/dashboard/Dashboard.js
@@ -12,16 +12,18 @@ import TPHead from '../TPHead/TPHead.js'
 class Dashboard extends Component{
 	render(){
 		return(
-			<div className = 'dashboard-box'>
+			<div className = 'dashboard-page'>
 				<header><TPHead/></header>
-				<div className='tp-head' style={{fontSize: 45, textAlign: 'left', alignSelf: 'stretch'}}>Assignment 1</div>
-				<div className='dashboard-content'>
-					<div className='box-1'><TotalErrors numErrors="32"/></div>
-					<div className='box-2'><LeastErrors student1 = "Utsav"/></div>
-					<div className='box-3'><MostErrors student1="Utsav"/></div>
-					<div className='box-4'><ErrorsPerQuestion/></div>
-					<div className='box-5'><ErrorType/></div>
-					<div className='box-6'><AverageTime/></div>
+				<div className = 'dashboard-box'>
+					<div className='tp-head' style={{fontSize: 45, textAlign: 'left', alignSelf: 'stretch'}}>Assignment 1</div>
+					<div className='dashboard-content'>
+						<div className='box-1'><TotalErrors numErrors="32"/></div>
+						<div className='box-2'><LeastErrors student1 = "Utsav"/></div>
+						<div className='box-3'><MostErrors student1="Utsav"/></div>
+						<div className='box-4'><ErrorsPerQuestion/></div>
+						<div className='box-5'><ErrorType/></div>
+						<div className='box-6'><AverageTime/></div>
+					</div>
 				</div>
 			</div>
 		);
