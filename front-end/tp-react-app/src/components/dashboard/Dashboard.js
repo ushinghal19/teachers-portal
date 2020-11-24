3435a8357edfcf3048339d1b9d9fc2cbe9d74133
Omri Daniel <omri.daniel@mail.utoronto.ca>
2020-11-23 22:46:08 -0500

    Moved API call function into dashboard component

--- front-end/tp-react-app/src/components/dashboard/Dashboard.js
+++ front-end/tp-react-app/src/components/dashboard/Dashboard.js
@@ -9,18 +9,31 @@ import ErrorType from './ErrorType.js'
 import AverageTime from './AverageTime.js'
 import TPHead from '../TPHead/TPHead.js'
 
+const URL = 'http://127.0.0.1:8000/graphql';
+let statistics = {};
 class Dashboard extends Component{
+	getAssignmentStats(id){
+		fetch(URL, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json'},
+			body: JSON.stringify({ query: 'getAssignmentStats($id: ID!){assignment(id: $id){studentsByErrors,typeOfErrors,problemErrors,aggregateErrors}}',
+									variables: {"id": id}
+			}),
+		})
+		.then(res => res.json())
+		.then(res => console.log(res.data))
+		.then(res => {statistics=JSON.parse(res.json())});
+	}
 	render(){
-		getAssignmentStats(321);
 		return(
 			<div className = 'dashboard-page'>
 				<header><TPHead/></header>
 				<div className = 'dashboard-box'>
-					<div className='tp-head' style={{fontSize: 45, textAlign: 'left', alignSelf: 'stretch'}}>Assignment 1</div>
+					<div className='tp-head' style={{fontSize: 45, textAlign: 'left', alignSelf: 'stretch', color: '#252525'}}>Assignment 1</div>
 					<div className='dashboard-content'>
-						<div className='box-1'><TotalErrors numErrors="32"/></div>
-						<div className='box-2'><LeastErrors student1 = "Utsav"/></div>
-						<div className='box-3'><MostErrors student1="Utsav"/></div>
+						<div className='box-1'><TotalErrors numErrors ={32}/></div>
+						<div className='box-2'><LeastErrors student1 = 'Utsav'/></div>
+						<div className='box-3'><MostErrors student1 = 'Utsav'/></div>
 						<div className='box-4'><ErrorsPerQuestion/></div>
 						<div className='box-5'><ErrorType/></div>
 						<div className='box-6'><AverageTime/></div>
@@ -29,19 +42,6 @@ class Dashboard extends Component{
 			</div>
 		);
 	}
-}
-
-const URL = 'http://127.0.0.1:8000/graphql'
-function getAssignmentStats(id){
-	fetch(URL, {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json'},
-		body: JSON.stringify({ query: 'getAssignmentStats($id: ID!){assignment(id: $id){studentsByErrors,typeOfErrors,problemErrors,aggregateErrors}}',
-								variables: {"id": id}
-		}),
-	  })
-	  .then(res => res.json())
-	  .then(res => console.log(res.data));
 }
 
 export default Dashboard;
