1607772948187b12209cb90b1ee9872e64bf3b52
Utsav Shinghal <ushinghal19@gmail.com>
2020-11-24 13:00:07 +0800

    working bar graph

--- front-end/tp-react-app/src/components/dashboard/ErrorsPerQuestion.js
+++ front-end/tp-react-app/src/components/dashboard/ErrorsPerQuestion.js
@@ -3,15 +3,35 @@ import './ErrorsPerQuestion.scss'
 import '../login/LoginPage.scss'
 import * as V from 'victory';
 
-const data = [{"Question Number": 0, "Number of Errors": 0},
-                {"Question Number": 1, "Number of Errors": 5}, 
-                {"Question Number": 2, "Number of Errors": 3},
-                {"Question Number": 3, "Number of Errors": 2},
-                {"Question Number": 4, "Number of Errors": 7},
-                {"Question Number": 5, "Number of Errors": 2},
-                {"Question Number": 6, "Number of Errors": 1},
-                {"Question Number": 7, "Number of Errors": 4},
-                {"Question Number": 8, "Number of Errors": 5}]
+const data = [{0: 0},
+                {1: 5}, 
+                {2: 3},
+                {3: 2},
+                {4: 7},
+                {5: 10},
+                {6: 1},
+                {7: 4},
+                {8: 12},
+                {9: 3},
+                {10: 6},
+                {11: 9},]
+
+
+function makeDataUsable(data){
+    let newObj = [];
+    for(let i=0; i<data.length; i++){
+        newObj.push({"x":i, "y":data[i][i]});
+    }
+    return(newObj);
+}
+
+function getTicks(data){
+    let newObj = [];
+    for(let i=0; i<data.length; i++){
+        newObj.push(i);
+    }
+    return(newObj);
+}
 
 class ErrorsPerQuestion extends Component {
     render() {
@@ -24,24 +44,44 @@ class ErrorsPerQuestion extends Component {
                         domainPadding={10}  
                         width={1000}
                     >
-                        <V.VictoryAxis
-                            style={{
-                                grid: { stroke: "#818e99", strokeWidth: 0.5 },
-                            }}
-                        />
+                    <V.VictoryAxis crossAxis
+                        width={400}
+                        height={400}
+                        domain={[0, 8]}
+                        theme={V.VictoryTheme.material}
+                        style={{
+                            axisLabel: {fontSize: 20, padding: 30},
+                            ticks: {stroke: "grey", size: 5},
+                            tickLabels: {fontSize: 15, padding: 4},
+                        }}
+                        tickValues = {getTicks(data)}
+                        label = "Question Number"
+                    />
+                    <V.VictoryAxis dependentAxis crossAxis
+                        width={400}
+                        height={400}
+                        domain={[0, 8]}
+                        theme={V.VictoryTheme.material}
+                        style={{
+                            axisLabel: {fontSize: 20, padding: 30},
+                            ticks: {stroke: "grey", size: 5},
+                            tickLabels: {fontSize: 15, padding: 4},
+                        }}
+
+                        label = "Number of Errors"
+                    />
                         <V.VictoryBar
-                            style={{ data: { fill: "#c43a31" } }}
-                            data={data}
-                            x = "Question Number"
-                            y = "Number of Errors"
+                            data={makeDataUsable(data)}
+                            // x = {Object.keys(data)}
+                            // y = {Object.values(data)}
                             animate={{
                                 duration: 4000,
                                 onLoad: { duration: 1500 }
                               }}
                             barRatio={.5}
                         />
-                        </V.VictoryChart>
-  }}
+                    </V.VictoryChart>
+
                  </div>
             </div>
         );
