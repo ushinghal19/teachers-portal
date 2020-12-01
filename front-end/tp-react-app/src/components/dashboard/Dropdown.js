625f2f4f5d9b6f04e6ea6afa0ea079f629accef2
Omri Daniel <omri.daniel@mail.utoronto.ca>
2020-12-01 18:33:47 -0500

    fixed 400 request error
    completed dropdown feature menu after fixing the server errors

--- front-end/tp-react-app/src/components/dashboard/Dropdown.js
+++ front-end/tp-react-app/src/components/dashboard/Dropdown.js
@@ -11,23 +11,13 @@ class Dropdown extends Component{
       super(props);
       this.state = {
         selectedOption: null,
-        options: [
-          {value: '0', label: 'Test Assignment'},
-        ],
+        options: [],
         teacherId: '5fb6d6ce00c239d5bffb4b15',
       };
     }
     handleChange = selectedOption => {
         this.setState({ selectedOption });
-        console.log(`Option selected:`, selectedOption);
       };
-
-    // content() {
-    //   const select = this.state.selectedOption;
-    //   if (select == null) {
-    //     return <div>Please select an assignment to view statistics!</div>
-    //   }
-    // }
 
     getTeacherAssignments(id){
       fetch(URL, {
@@ -37,19 +27,19 @@ class Dropdown extends Component{
           "Referer": "http://localhost:3000",
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ query: 'query getTeacherAssignments($id: ID!){teacher(id: $id){assignments_below{assignment_id}}}',
+        body: JSON.stringify({ query: 'query getTeacherAssignments($id: ID!){teachers(id: $id){assignmentsBelow{assignmentId}}}',
           variables: {"id": id},
           operationName: "getTeacherAssignments",
         }),
       })
         .then(res => res.json())
         .then((result) => {
-                  var x = result.data.teacher.assignments_below;
-                  for (let i = 0; i < x.length; i++) {
-                    this.setState(prevState => ({
-                      objects: [...prevState.objects, {value: x[i].assignment_id, label: x[i].assignment_id}]
-                    }));
-                  }
+                  var TeacherAssignments = result.data.teachers[0].assignmentsBelow;
+                  var allAssignments = [];
+                  for (let i = 0; i < TeacherAssignments.length; i++) {
+                    allAssignments.push({value: TeacherAssignments[i].assignmentId, label: TeacherAssignments[i].assignmentId})
+                    }
+                    this.setState({options: allAssignments})
                 },
                 (error) => {
                   this.setState({
@@ -87,9 +77,8 @@ class Dropdown extends Component{
           </div>
           {content}
         </div>
-        
       );
     }
 }
 
-export default Dropdown
+export default Dropdown;
