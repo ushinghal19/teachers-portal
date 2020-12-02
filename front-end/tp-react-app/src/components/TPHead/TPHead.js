3c0f78ad7edcc50754b6fa3002f28a4975c578b9
Omri Daniel <omri.daniel@mail.utoronto.ca>
2020-12-01 19:52:38 -0500

    minor style changing
    title changed, and some padding

--- front-end/tp-react-app/src/components/TPHead/TPHead.js
+++ front-end/tp-react-app/src/components/TPHead/TPHead.js
@@ -5,7 +5,7 @@ import '../login/LoginPage.scss'
 class TPHead extends Component {
     render() {
         return (
-            <div className="tp-head tp-navbar" style={{fontSize: 35,}}>
+            <div className="tp-head tp-navbar" style={{fontSize: 35,color:"black"}}>
                 Teacher's Portal
             </div>
         );
